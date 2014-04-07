d7f42e276f6efc4b45e5c9a60646e4f3b4949f63
Anthony Spring <aspring@yieldbot.com>
2014-04-07 13:45:13 -0400

    Bump nodejs version to 0.10.26

--- cookbooks/nodejs/attributes/default.rb
+++ cookbooks/nodejs/attributes/default.rb
@@ -5,6 +5,7 @@ default[:nodejs][:install_dir] = "/usr/src/nodejs"
 default[:nodejs][:bin_path] = "/usr/local/bin/node"
 
 # these attributes used when installing from release bin tarball
-default[:nodejs][:release_url] = "http://nodejs.org/dist/v0.10.5/node-v0.10.5-linux-x64.tar.gz"
-default[:nodejs][:home_dir] = "/usr/local/share/nodejs-0.10.5"
-default[:nodejs][:version] = "0.10.5"
+default[:nodejs][:release_url] = "http://nodejs.org/dist/v0.10.26/node-v0.10.26-linux-x64.tar.gz"
+
+default[:nodejs][:home_dir] = "/usr/local/share/nodejs-0.10.26"
+default[:nodejs][:version] = "0.10.26"
