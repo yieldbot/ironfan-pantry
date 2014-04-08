36c334bed505d87ca98e6289a527bca426bd36ac
Anthony Spring <aspring@yieldbot.com>
2014-04-08 10:14:33 -0400

    Add a basic sensu check to ES cookbook for the initial integration test

---
+++ cookbooks/elasticsearch/recipes/sensu_checks.rb
@@ -0,0 +1,19 @@
+# Only process if we find the monitor cookbook in the run list
+return unless node.recipe?('monitor')
+
+# Bring in the necessary ruby dependencies
+sensu_gem 'rest-client'
+
+# Monitor the cluster status
+sensu_check 'check-es-server-cluster-status' do
+  command   "#{node['sensu']['directories']['base']}/plugins/sensu-community-plugins/elasticsearch/check-es-server-cluster-status.rb"
+  standalone    true
+end
+
+# TODO: Calculate the warning/critical heap bytes based off of the current number of bytes allocated to the JVM
+
+# # Monitor the cluster JVM heap
+# sensu_check 'check-es-heap' do
+#   command   "#{node['sensu']['directories']['base']}/plugins/sensu-community-plugins/elasticsearch/check-es-heap.rb -w #{warning} -c #{critical}"
+#   standalone    true
+# end
