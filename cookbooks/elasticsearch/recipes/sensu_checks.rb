c2d111502ec7ec1771531618c11bfea987a2ba04
Anthony Spring <aspring@yieldbot.com>
2014-04-11 17:00:48 -0400

    Add shard allocation status check

--- cookbooks/elasticsearch/recipes/sensu_checks.rb
+++ cookbooks/elasticsearch/recipes/sensu_checks.rb
@@ -11,6 +11,13 @@ sensu_check 'check-es-cluster-status' do
   standalone  true
 end
 
+# Monitor the shard allocation status
+sensu_check 'check-es-shard-allocation-status' do
+  command     "#{node['sensu']['directories']['base']}/plugins/sensu-yieldbot-plugins/elasticsearch/check-es-shard-allocation-status.rb"
+  handlers    ['devops-orange']
+  standalone  true
+end
+
 # TODO: Calculate the warning/critical heap bytes based off of the current number of bytes allocated to the JVM
 
 # # Monitor the cluster JVM heap
