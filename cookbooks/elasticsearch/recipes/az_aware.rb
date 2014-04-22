51573e2ad3c891b0e98be607d806188578d09ad6
Anthony Spring <aspring@yieldbot.com>
2014-04-22 19:31:39 -0400

    Expand AZ awareness to explicitly set the rack_id as an awareness attribute

--- cookbooks/elasticsearch/recipes/az_aware.rb
+++ cookbooks/elasticsearch/recipes/az_aware.rb
@@ -1,2 +1,8 @@
 # Add this recipe to the run list to make this node AZ aware
-node.default[:elasticsearch][:custom_config] =  {'node.rack_id' => "#{node[:ec2][:placement_availability_zone]}"}
+node.default[:elasticsearch][:custom_config] =  {
+  # Set the rack_id to the availability zone
+  'node.rack_id' => "#{node[:ec2][:placement_availability_zone]}"
+
+  # Tell the awareness subsystem to utilize the rack_id attribute
+  'cluster.routing.allocation.awareness.attributes' => 'rack_id'
+}
