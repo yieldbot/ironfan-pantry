4844944d67788a9bdc9612eebc8db5ba7063432b
Anthony Spring <aspring@yieldbot.com>
2014-04-22 21:11:00 -0400

    Comma!

--- cookbooks/elasticsearch/recipes/az_aware.rb
+++ cookbooks/elasticsearch/recipes/az_aware.rb
@@ -1,7 +1,7 @@
 # Add this recipe to the run list to make this node AZ aware
 node.default[:elasticsearch][:custom_config] =  {
   # Set the rack_id to the availability zone
-  'node.rack_id' => "#{node[:ec2][:placement_availability_zone]}"
+  'node.rack_id' => "#{node[:ec2][:placement_availability_zone]}",
 
   # Tell the awareness subsystem to utilize the rack_id attribute
   'cluster.routing.allocation.awareness.attributes' => 'rack_id'
