9ba3ecf57dcf5061c908546365cdca6955f22169
Anthony Spring <aspring@yieldbot.com>
2014-09-12 15:58:58 -0400

    Add check to the consul service

--- cookbooks/elasticsearch/recipes/consul_service.rb
+++ cookbooks/elasticsearch/recipes/consul_service.rb
@@ -1,6 +1,10 @@
 include_recipe 'devops_consul::default'
 
 devops_consul_service 'elasticsearch' do
-  tags      node['elasticsearch']['consul']['tags']
-  port      9200
+  tags          node['elasticsearch']['consul']['tags']
+  port          9200
+  create_check  true
+  type          'interval'
+  interval      '10s'
+  script        'curl localhost:9200 >/dev/null 2>&1'
 end
