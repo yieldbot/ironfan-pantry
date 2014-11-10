82d2e386bf808ec8d8356e811fa49e29da5b7f65
Anthony Spring <aspring@yieldbot.com>
2014-11-10 09:42:08 -0500

    Break down check into its own recipe for tribe nodes

--- cookbooks/elasticsearch/recipes/sensu_checks.rb
+++ cookbooks/elasticsearch/recipes/sensu_checks.rb
@@ -20,13 +20,3 @@ sensu_check 'check-es-shard-allocation-status' do
   handlers    ['devops-orange']
   standalone  true
 end
-
-# Add in the index duplicate monitoring checks if defined
-sensu_check 'check-es-cluster-index' do
-  cluster = "--cluster #{node[:elasticsearch][:sensu][:check_es_cluster_index][:servers].join(',')}" if node[:elasticsearch][:sensu][:check_es_cluster_index][:servers].length > 0
-  ignore  = "--ignore #{node[:elasticsearch][:sensu][:check_es_cluster_index][:ignore].join(',')}" if node[:elasticsearch][:sensu][:check_es_cluster_index][:ignore].length > 0
-
-  command     "#{node['sensu']['directories']['base']}/plugins/sensu-yieldbot-plugins/elasticsearch/check-es-indexes.rb #{cluster} #{ignore}"
-  handlers    ['devops-red']
-  standalone  true
-end if node[:elasticsearch][:sensu][:check_es_cluster_index][:servers].length > 0
---
+++ cookbooks/elasticsearch/recipes/sensu_checks_tribe.rb
@@ -0,0 +1,12 @@
+# Only process if we find the monitor cookbook in the run list
+return unless node.recipe?('monitor::default')
+
+# Add in the index duplicate monitoring checks if defined
+sensu_check 'check-es-cluster-index' do
+  cluster = "--cluster #{node[:elasticsearch][:sensu][:check_es_cluster_index][:servers].join(',')}" if node[:elasticsearch][:sensu][:check_es_cluster_index][:servers].length > 0
+  ignore  = "--ignore #{node[:elasticsearch][:sensu][:check_es_cluster_index][:ignore].join(',')}" if node[:elasticsearch][:sensu][:check_es_cluster_index][:ignore].length > 0
+
+  command     "#{node['sensu']['directories']['base']}/plugins/sensu-yieldbot-plugins/elasticsearch/check-es-indexes.rb #{cluster} #{ignore}"
+  handlers    ['devops-red']
+  standalone  true
+end if node[:elasticsearch][:sensu][:check_es_cluster_index][:servers].length > 0
