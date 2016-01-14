c3f8d180ea69398fbd4adfc66366fd8051149f1b
Kunal Nawale <knawale@yieldbot.com>
2016-01-13 22:06:47 -0500

    fix comma

--- cookbooks/elasticsearch/recipes/nodelogstash.rb
+++ cookbooks/elasticsearch/recipes/nodelogstash.rb
@@ -21,7 +21,7 @@ log_dir      = node[:elasticsearch][:log_dir]
 devops_nodelogstash_config "#{cluster_name}" do
   urls [
     "input://file://#{log_dir}/#{cluster_name}.log?type=elasticsearch",
-    'filter://regex://?only_type=elasticsearch&regex=\[([\s\S]+)\]\[(\S+)\s*\]\[(\S+)\s*\] \[(\S+)\] ([\s\S]+)&date_format=YYYY-MM-DD HH:mm:ss,SSS&fields=@timestamp,loglevel,class,es_node,message'
+    'filter://regex://?only_type=elasticsearch&regex=\[([\s\S]+)\]\[(\S+)\s*\]\[(\S+)\s*\] \[(\S+)\] ([\s\S]+)&date_format=YYYY-MM-DD HH:mm:ss,SSS&fields=@timestamp,loglevel,class,es_node,message',
     'filter://truncate://?max_size=999000'
   ]
 end
@@ -29,7 +29,7 @@ end
 devops_nodelogstash_config "#{cluster_name}_slowlog" do
   urls [
     "input://file://#{log_dir}/#{cluster_name}_index_search_slowlog.log?type=elasticsearch_slowlog",
-    'filter://regex://?only_type=elasticsearch_slowlog&regex=\[([\s\S]+)\]\[(\S+)\]\[(\S+)\s*\] \[(\S+)\] \[(\S+)\]\[(\d+)\] took\[(\S+)\], took_millis\[(\d+)\], types\[(\S*)\], stats\[(\S*)\], search_type\[(\S+)\], total_shards\[(\d+)\], source\[([\s\S]*)\], extra_source\[(\S*)\]&date_format=YYYY-MM-DD HH:mm:ss,SSS&fields=@timestamp,loglevel,class,es_node,index,shard,took,took_ms,types,stats,search_type,total_shards,query_source,extra_source'
+    'filter://regex://?only_type=elasticsearch_slowlog&regex=\[([\s\S]+)\]\[(\S+)\]\[(\S+)\s*\] \[(\S+)\] \[(\S+)\]\[(\d+)\] took\[(\S+)\], took_millis\[(\d+)\], types\[(\S*)\], stats\[(\S*)\], search_type\[(\S+)\], total_shards\[(\d+)\], source\[([\s\S]*)\], extra_source\[(\S*)\]&date_format=YYYY-MM-DD HH:mm:ss,SSS&fields=@timestamp,loglevel,class,es_node,index,shard,took,took_ms,types,stats,search_type,total_shards,query_source,extra_source',
     'filter://truncate://?max_size=999000'
   ]
 end
